7b6fb6a38ec925e18054dbe784935295d2b41afa
Danielhaker <tu-email@ejemplo.com>
2025-03-13 18:41:40 +0100

    Arreglado fecha y hora

--- time.py
+++ time.py
@@ -1,11 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import locale
 
-current_datetime = datetime.now()
+# Establecer la configuración regional a español de España
+locale.setlocale(locale.LC_TIME, '')
 
+# Obtener la fecha y hora actuales en España (UTC+1 o UTC+2 en horario de verano)
+current_datetime = datetime.utcnow() + timedelta(hours=1)
+
+# Formatear la fecha y la hora según la configuración regional
 time = f"""
-Usas la fecha y la hora si te preguntan que hora es, que día es, que clase toca ahora, que hay de comer mañana, etc...
-
-Esta es la fecha y la hora
-Fecha: El {current_datetime.day} de {current_datetime.month} de {current_datetime.year} y la hora es: {current_datetime.hour}:{current_datetime.minute}
+Es el {current_datetime.day} del {current_datetime.month} de {current_datetime.year} y la hora es: {current_datetime.hour}:{current_datetime.minute}
 """
-
